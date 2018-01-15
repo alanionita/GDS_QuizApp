8e621de190a127abd08d3271b111724f19689ba9
alanionita <a.g.ionita@gmail.com>
2018-01-15 19:05:39 +0000

    adds initial scoring logic and UI trigger for the first quiz question

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int correctGuesses = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitAnswers(View view) {
+        countCorrectGuesses();
+        CharSequence text = "You have " + correctGuesses + " correct guesses!" + "\nWell done!";
+        if (correctGuesses == 0) {
+            text = "Wow! You're not very good at this are you?!!!";
+        } else if (correctGuesses == 1) {
+            text = "You have only " + correctGuesses + " correct guess!" + "\nThis must be a hard quiz!";
+        }
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        resetQuiz();
+    }
+
+    private Boolean questionOneCorrectChoice() {
+        final RadioButton answerC = (RadioButton) findViewById(R.id.q1_c);
+        if (answerC.isChecked()) return true;
+        else return false;
+    }
+
+    private void countCorrectGuesses() {
+
+        if (questionOneCorrectChoice() == true) correctGuesses++;
+    }
+
+    public void resetQuiz () {
+        correctGuesses = 0;
+    }
+
+
+
 }
